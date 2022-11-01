19d66654527b64d85c53dd7efc12cda210eeab82
Panagiotis Spanakis <t8200158@aueb.gr>
2022-11-01 12:57:31 +0200

    Page again

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,5 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function dropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
 (function ($) {
     "use strict";
-
-    $(function () {
-        var dtToday = new Date();
-
-        var month = dtToday.getMonth() + 1;
-        var day = dtToday.getDate();
-        var year = dtToday.getFullYear();
-        if (month < 10)
-            month = '0' + month.toString();
-        if (day < 10)
-            day = '0' + day.toString();
-
-        var maxDate = year + '-' + month + '-' + day;
-        $('#startdateId').attr('min', maxDate);
-
-
-        if (month < 10) month = "0" + month;
-        if (day < 10) day = "0" + day;
-
-        var today = year + "-" + month + "-" + day;
-
-        document.getElementById("startdateId").value = today;
-    });
-
-
-    /* Preloader */
-    $(window).on('load', function () {
-        var preloaderFadeOutTime = 500;
-
-        function hidePreloader() {
-            var preloader = $('.spinner-wrapper');
-            setTimeout(function () {
-                preloader.fadeOut(preloaderFadeOutTime);
-            }, 500);
-        }
-
-        hidePreloader();
-    });
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function () {
-        $(document).on('click', 'a.page-scroll', function (event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 600, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    });
-
     //Inactivity function
     var idleTime = 0;
     $(document).ready(function () {
@@ -82,15 +16,10 @@ function dropdown() {
 
     function timerIncrement() {
         idleTime = idleTime + 1;
-        if (idleTime > 20) { // 20 minutes
-            alert("You have been inactive for 20 minutes\nSite will be reloaded")
+        if (idleTime > 14) { // 15 minutes
+            alert("You have been inactive for 15 minutes\nSite will be reloaded")
             window.location.reload();
         }
     }
-
-    /* Removes Long Focus On Buttons */
-    $(".button, a, button").mouseup(function () {
-        $(this).blur();
-    });
 
 })(jQuery);
